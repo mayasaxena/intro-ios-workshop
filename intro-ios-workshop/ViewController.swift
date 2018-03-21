7e406dc91b85b71b15109b2d73adbf690b5882d1
Maya Saxena <maya.saxena@gmail.com>
2018-03-21 14:27:03 -0400

    Basic Magic 8 Ball app

--- intro-ios-workshop/ViewController.swift
+++ intro-ios-workshop/ViewController.swift
@@ -10,16 +10,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let responses = [
+        "It is certain",
+        "It is decidedly so",
+        "Without a doubt",
+        "Yes definitely",
+        "You may rely on it",
+        "As I see it, yes",
+        "Most likely",
+        "Outlook good",
+        "Yes",
+        "Signs point to yes",
+        "Reply hazy try again",
+        "Ask again later",
+        "Better not tell you now",
+        "Cannot predict now",
+        "Concentrate and ask again",
+        "Don't count on it",
+        "My reply is no",
+        "My sources say no",
+        "Outlook not so good",
+        "Very doubtful"
+    ]
+
+    @IBOutlet weak var responseLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func handleShakeButtonPressed(_ sender: Any) {
+        responseLabel.text = getRandomResponse()
     }
 
+    private func getRandomResponse() -> String {
+        let randomIndex = Int(arc4random_uniform(UInt32(responses.count)))
+        return responses[randomIndex]
+    }
 
 }
-
